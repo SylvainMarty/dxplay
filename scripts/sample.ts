d8bdb72b421e1e150c276ce18479d020d23b543f
SylvainMarty <sylvain.marty@aol.fr>
2020-11-14 19:36:45 +0100

    New try

--- scripts/sample.ts
+++ scripts/sample.ts
@@ -64,13 +64,44 @@ const displayOnMatrix = async (text: string): Promise<void> => {
         for (const alignmentV of [VerticalAlignment.Top, VerticalAlignment.Middle, VerticalAlignment.Bottom]) {
             matrix.fgColor(Colors.Yellow).clear();
             LayoutUtils.linesToMappedGlyphs(lines, font.height(), matrix.width(), matrix.height(), alignmentH, alignmentV)
-            .map(glyph => {
-                matrix.drawText(glyph.char, glyph.x, glyph.y);
-            });
+                .map(glyph => {
+                    matrix.drawText(glyph.char, glyph.x, glyph.y);
+                });
             matrix.sync();
             await wait(400);
         }
     }
 }
 
-displayOnMatrix(process.argv[2]);
+const sample = async () => {
+    const matrix = buildMatrix();
+
+    matrix
+        .clear()            // clear the display
+        .brightness(100)    // set the panel brightness to 100%
+        .fgColor(0x0000FF)  // set the active color to blue
+        .fill()             // color the entire diplay blue
+        .fgColor(0xFFFF00)  // set the active color to yellow
+        // draw a yellow circle around the display
+        .drawCircle(matrix.width() / 2, matrix.height() / 2, matrix.width() / 2 - 1)
+        // draw a yellow rectangle
+        .drawRect(matrix.width() / 4, matrix.height() / 4, matrix.width() / 2, matrix.height() / 2)
+        // sets the active color to red
+        .fgColor({ r: 255, g: 0, b: 0 })
+        // draw two diagonal red lines connecting the corners
+        .drawLine(0, 0, matrix.width(), matrix.height())
+        .drawLine(matrix.width() - 1, 0, 0, matrix.height() - 1);
+
+    matrix.sync();
+
+    await wait(999999999);
+}
+
+const value = process.argv[2];
+if (!value) {
+    console.log('You must provide a text in argument!')
+    process.exit(1)
+} else {
+    // displayOnMatrix(process.argv[2]);
+    sample();
+}
